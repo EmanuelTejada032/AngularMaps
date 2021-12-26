a2d3d21f67ce1fab6d34cdd4e2c0e6048a92f0d8
EMANUEL <emanueltejada31295@gmail.com>
2021-12-26 12:04:23 -0400

    Add zoom range map, add local reference, get local reference using ViewChild

--- src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+    .map-container{
+      margin-top: 30px;
+      height: 70%;
+      width: 70%;
+    }
+    .row{
+      background-color: white;
+      padding: 10px;
+      z-index: 1
+    }
+    `
   ]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @ViewChild('zoomRangeMap') mapDiv!: ElementRef // get local reference on html template
+  map!: mapboxgl.Map;
+  constructor() {}
+  
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.mapDiv.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-69.81783771668067,18.526597292645913],
+      zoom: 15
+    });
   }
 
+  zoomIn(){
+    console.log('zoomIn', this.mapDiv);
+    this.map.zoomIn()
+  }
+
+  zoomOut(){
+    console.log('zoomOutm', this.mapDiv);
+    this.map.zoomOut();
+  }
 }
